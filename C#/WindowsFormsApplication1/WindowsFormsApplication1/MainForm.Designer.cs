85f13c5d210a023f7cb500cfdf806add79227927
DavidUMAGitHub <davidmunvalle@gmail.com>
2024-11-19 22:10:08 +0100

    Añado las cosas de C#

---
+++ C#/WindowsFormsApplication1/WindowsFormsApplication1/MainForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Productos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.gestionarProducotsClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(350, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Atributos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.gestionarAtributosClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(611, 239);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 61);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Cetegorías";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.gestionarCategoriasClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(221, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(418, 120);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PLYTIX";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 526);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ C#/WindowsFormsApplication1/WindowsFormsApplication1/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void gestionarProducotsClick(object sender, EventArgs e)
+        {
+            var productosForm = new GestionProductosForm();
+            productosForm.Show();
+            this.Hide();
+        }
+
+        private void gestionarCategoriasClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gestionarAtributosClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
