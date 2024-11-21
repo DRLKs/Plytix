8e097544f0d8afd46b6b40458316d0dd42f8ba2a
DRLK <davidmunvalle@gmail.com>
2024-11-21 16:25:19 +0100

    Elimino Archivos Desfasados

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('C:\Users\David\Desktop\IngenieriaRequisitos.db') or 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
